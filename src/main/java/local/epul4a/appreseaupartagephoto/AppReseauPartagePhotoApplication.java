08f608a09a07a1d432d7ffe1cd8b2decfb3c551e
Eclairsombre <alexmt14095@gmail.com>
2025-11-28 10:53:52 +0100

    Initial commit

---
+++ src/main/java/local/epul4a/appreseaupartagephoto/AppReseauPartagePhotoApplication.java
@@ -0,0 +1,13 @@
+package local.epul4a.appreseaupartagephoto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppReseauPartagePhotoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppReseauPartagePhotoApplication.class, args);
+    }
+
+}
